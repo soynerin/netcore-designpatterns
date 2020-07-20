fe90f2e70704904eec4e4346ff61097011b99d71
soyNerin <neri.agustin.es@outlook.com>
2020-07-20 20:40:00 -0300

    Ejemplo de uso del patrón Singleton

---
+++ Singleton/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var singleton = Singleton.ObtenerInstancia();
+            singleton.InsertarDatos("Neri", 30);
+            Console.WriteLine(singleton);
+
+            Console.WriteLine("----------");
+
+            var otroSingleton = Singleton.ObtenerInstancia();
+            Console.WriteLine(otroSingleton);
+        }
+    }
+}
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Singleton
+{
+    internal class Singleton
+    {
+        private static Singleton instancia;
+        private string nombre;
+        private int edad;
+
+        private Singleton()
+        {
+            nombre = "Sin asignar";
+            edad = 99;
+        }
+
+        public static Singleton ObtenerInstancia()
+        {
+            if (instancia == null)
+            {
+                instancia = new Singleton();
+
+                Console.WriteLine("Instancia creada por primera vez");
+            }
+
+            return instancia;
+        }
+
+        public override string ToString()
+        {
+            return $"La persona {nombre} tiene {edad} años";
+        }
+
+        public void InsertarDatos(string nombre, int edad)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+        }
+    }
+}
